c83b838334daf559d0ff527deec45811e96adacc
Matt Sharp <matthewssharp1@gmail.com>
2018-05-26 13:47:49 +0000

    Added new route to Create Note in Navbar, changed About page

--- ElevenNote.Web/Controllers/HomeController.cs
+++ ElevenNote.Web/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ElevenNote.Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Welcome to The Note App.";
 
             return View();
         }
